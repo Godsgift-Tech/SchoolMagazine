8620f772df5c73a1dadbd1230f700756500b304b
Godsgift Oghenechohwo <godsgiftoghenechohwo@gmail.com>
2025-01-17 11:26:25 +0100

    This is the Development branch of a School Magazine

---
+++ SchoolMagazine.Domain/Entities/School.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMagazine.Domain.Entities
+{
+    public class School
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Location { get; set; }
+        [Required]
+        public string WebsiteUrl { get; set; }
+        [Required]
+        public decimal FeesRange { get; set; }
+        [Required]
+        public double Rating { get; set; }
+        public ICollection<SchoolEvent> Events { get; set; }
+    }
+}
---
+++ SchoolMagazine.Domain/Entities/SchoolAdvert.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolMagazine.Domain.Entities
+{
+    public class SchoolAdvert
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+        public Guid SchoolId { get; set; }
+        public School School { get; set; }
+    }
+}
